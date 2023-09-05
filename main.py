9fbd56c8cd6a08d857a6e96b9757b7f886634f95
cgeissler <cgeissler@advestis.com>
2023-09-05 16:15:06 +0200

    new: dev: creation of a main module to test pypdf2

---
+++ main.py
@@ -0,0 +1,18 @@
+# from langchain.document_loaders import PyPdfLoader
+# from tabula import read_pdf
+from PyPDF2 import PdfReader
+
+# Path: main.py
+
+
+if __name__ == "__main__":
+    data_path = "TestData/334064.pdf"
+    # tabula version
+    # data = read_pdf(data_path, pages="all", multiple_tables=True)
+    # pypdf2 version
+    reader = PdfReader(data_path)
+    page = reader.pages[3]
+    print(page.extract_text())
+
+
+
